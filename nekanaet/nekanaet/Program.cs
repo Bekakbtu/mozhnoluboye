21db5d1235265e70ef13466753b98f1ee1d7d507
Бекзат <macbook@MacBook-Pro-Beka.local>
2019-02-14 15:48:45 +0600

    poluchitsa li

---
+++ nekanaet/nekanaet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nekanaet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pokazhite mne moi kodid");
+        }
+    }
+}
